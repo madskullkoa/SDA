4eac4030e9608376229df0004d349edbbe71a18e
Ẏṏṳçḕf MặđSķůłl <you.neuffdeuxx@gmail.com>
2020-12-29 21:49:18 +0000

    tri.cpp et tri.h + retiré les fonctions de main.cpp

--- main.cpp
+++ main.cpp
@@ -3,62 +3,7 @@
   * @version 1 � 18/11/2020
   * @brief Types : co�t-m�moire et domaine de variation
   */
-#pragma warning(disable:4996)
-#include <iostream>
-#include <cstring>
-#include <cassert>
-#include "Liste.h"
-#include "Structs.h"
-#include "ConteneurTDE.h"
-#include "points.h"
-using namespace std;
 
-/**
- * @brief enleves les doublons
- * @param[in] c le conteneur des mots
- */
-void triUnique(ConteneurTDE& c, Joueurs& j, Item entree, int cpt) {
-	Mot tmp;
-	strcpy(tmp, entree);
-	for (int i = 0; i < j.nbdemot; ++i) {
-		if (strcmp(tmp, c.tab[i]) == 0) {
-			//cout << "Votre mot existe deja dans la base" << endl;
-			j.nbdemot--;
-			return;
-		}
-	}
-	ecrire(c, cpt, tmp);
-}
-
-/**
- * @brief Trier la liste de mots par ordre alphabetique
- * @param[in] entre le mot entré.
- * @param[in] c Le conteneur des mots
- * @param[in] j La structure Joueurs (sert a rien je crois)
- */
-void triAlpha(ConteneurTDE& c, Joueurs& jr) {
-
-	Mot tmp;
-
-	for (int i = 0; i < jr.nbdemot; i++) {
-		for (int j = 0; j < jr.nbdemot - i - 1; j++) {
-			if (strcmp(c.tab[j], c.tab[j + 1]) < 0) {
-
-			}
-			else if (strcmp(c.tab[j], c.tab[j + 1]) > 0)
-			{
-				strcpy(tmp, c.tab[j]);
-				strcpy(c.tab[j], c.tab[j + 1]);
-				strcpy(c.tab[j + 1], tmp);
-			}
-			else if (strcmp(c.tab[j], c.tab[j + 1]) == 0)
-			{
-
-			}
-		}
-	}
-
-}
 
 
 int main() {
---
+++ tri.cpp
@@ -0,0 +1,57 @@
+#pragma warning(disable:4996)
+#include <iostream>
+#include <cstring>
+#include <cassert>
+#include "Liste.h"
+#include "Structs.h"
+#include "ConteneurTDE.h"
+#include "points.h"
+#include "tri.h"
+using namespace std;
+
+/**
+ * @brief enleves les doublons
+ * @param[in] c le conteneur des mots
+ */
+void triUnique(ConteneurTDE& c, Joueurs& j, Item entree, int cpt) {
+	Mot tmp;
+	strcpy(tmp, entree);
+	for (int i = 0; i < j.nbdemot; ++i) {
+		if (strcmp(tmp, c.tab[i]) == 0) {
+			//cout << "Votre mot existe deja dans la base" << endl;
+			j.nbdemot--;
+			return;
+		}
+	}
+	ecrire(c, cpt, tmp);
+}
+
+/**
+ * @brief Trier la liste de mots par ordre alphabetique
+ * @param[in] entre le mot entré.
+ * @param[in] c Le conteneur des mots
+ * @param[in] j La structure Joueurs (sert a rien je crois)
+ */
+void triAlpha(ConteneurTDE& c, Joueurs& jr) {
+
+	Mot tmp;
+
+	for (int i = 0; i < jr.nbdemot; i++) {
+		for (int j = 0; j < jr.nbdemot - i - 1; j++) {
+			if (strcmp(c.tab[j], c.tab[j + 1]) < 0) {
+
+			}
+			else if (strcmp(c.tab[j], c.tab[j + 1]) > 0)
+			{
+				strcpy(tmp, c.tab[j]);
+				strcpy(c.tab[j], c.tab[j + 1]);
+				strcpy(c.tab[j + 1], tmp);
+			}
+			else if (strcmp(c.tab[j], c.tab[j + 1]) == 0)
+			{
+
+			}
+		}
+	}
+
+}
